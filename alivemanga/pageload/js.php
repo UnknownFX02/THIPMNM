88009408977e5b646350bd6c01e14d858ba1dfc4
Thanh Bui <mondorae2006@gmail.com>
2019-12-10 00:55:27 +0700

    xoa lan 2

--- alivemanga/pageload/js.php
+++
@@ -1,7 +0,0 @@
-<!-- JavaScripts -->
-<script src="js/jquery-3.4.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/bootstrap.bundle.js"></script>
-
